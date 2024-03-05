e7b3a693714ed4eb4c4ccbb1ba1f12891cd4673f
Nihadname <nihadcoding@gmail.com>
2024-03-05 20:03:47 +0400

    differentiate outputs of methods

--- assets/js/1.js
+++ assets/js/1.js
@@ -13,25 +13,24 @@ emptyArray.push(splitted[i].length);
 }
 return emptyArray
 }
-console.log(gettingItsLettersCount(str));
+console.log(`1ci usul : ${gettingItsLettersCount(str)}`);
 
 const gettingItsLettersCount2 = str.split(" ").map(myFunction);
 function myFunction(value) {
     return value.length;
 }
 
-console.log(gettingItsLettersCount2);
+console.log(`2ci usul : ${gettingItsLettersCount2}`);
 const gettingItsLettersCount3 = str.split(" ").map(value => value.length);
-console.log(gettingItsLettersCount3);
-let lengthArray=[];
+console.log(`3ci usul : ${gettingItsLettersCount3}`);let lengthArray=[];
 const gettingItsLettersCount4 = str.split(" ").forEach(value =>{
     lengthArray.push(value.length);
 });
 
-console.log(lengthArray);
+console.log(`4ci usul : ${lengthArray}`);
 
 let gettingItsLettersCount5=str.split(" ").reduce((initialValue,value)=>{
     initialValue.push(value.length);
     return initialValue;
 },[]);
-console.log(gettingItsLettersCount5);
+console.log(`5ci usul : ${gettingItsLettersCount5}`);
